4dadc2cb63cf9b8e606fd4789d465cf965cec4dd
Carolina Capetillo <carolinacapetillo@carolinas-mbp.lco.gtn>
2023-11-15 16:52:34 -0800

    added styling for images to be responsive depending on the size of screen but haven't styled that yet. just added the skeleton for it

--- src/components/ImageComponent.vue
+++ src/components/ImageComponent.vue
@@ -9,17 +9,38 @@
 
 <template>
     <v-carousel>
-        <v-carousel-item
-        v-for="(image, index) in images"
-        :key="index"
-        :src="image"
-        ></v-carousel-item>
-  </v-carousel>
+        <v-carousel-item v-for="(image, index) in images" :key="index">
+            <v-row justify="center">
+                <v-col cols="12" md="10" lg="8">
+                    <img :src="image" class="responsive-image" />
+                </v-col>
+            </v-row>
+        </v-carousel-item>
+    </v-carousel>
 </template>
 
 
 <style scoped>
-.v-carousel-item {
-  height: 400px;
+.responsive-image {
+  width: 100%;
+  height: auto;
+}
+
+@media (max-width: 600px) { /* Phones */
+  .responsive-image {
+    /* Styles for phones */
+  }
+}
+
+@media (min-width: 600px) and (max-width: 960px) { /* Tablets */
+  .responsive-image {
+    /* Styles for tablets */
+  }
+}
+
+@media (min-width: 960px) { /* Desktop */
+  .responsive-image {
+    /* Styles for desktop */
+  }
 }
 </style>
