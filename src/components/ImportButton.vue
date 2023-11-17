9f4335379a498f6c3a102caf494f9e3c2d00213c
Carolina Capetillo <carolinacapetilloc@gmail.com>
2023-11-17 13:13:36 -0800

    passing down props

---
+++ src/components/ImportButton.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { inject } from 'vue';
+
+const router = useRouter();
+const selectedImages = inject('selectedImages');
+
+const goToDataSession = () => {
+  router.push({ 
+    name: 'DataSession', 
+    params: { selectedImages: JSON.stringify(selectedImages.value) }
+  });
+};
+</script>
+
+<template>
+  <v-btn @click="goToDataSession">Import</v-btn>
+</template>
