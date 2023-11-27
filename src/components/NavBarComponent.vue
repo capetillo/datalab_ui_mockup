cfbe87c3f15ea8dadbac8f2e0e3008b916062e13
Carolina Capetillo <carolinacapetilloc@gmail.com>
2023-11-27 10:45:15 -0800

    Added skeleton of navbar with ugly css

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <router-link to="/projects">Projects</router-link>
+    <router-link to="/datasession">Data Session</router-link>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: red;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-around;
+}
+
+router-link {
+  color: white;
+  text-decoration: none;
+}
+
+/* Style for the active route */
+.router-link-active {
+  font-weight: bold;
+}
+</style>
