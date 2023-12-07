e68fef50ffbe629813c6e1e3376a711c1ffbefeb
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-07 14:13:58 -0800

    added theme switch for dark/light

---
+++ src/components/ThemeSwitch.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import { useTheme } from 'vuetify'
+    const theme = useTheme()
+    function toggleTheme () {
+        theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+    }
+</script>
+
+<template>
+    <v-switch label="Dark Mode" inset @change="toggleTheme()"></v-switch>
+</template>
