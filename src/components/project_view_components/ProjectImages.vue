6d848f844c65c2842249e84260a8aa2544d8b9e2
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-08 14:58:20 -0800

    Basic Scafolding for ProjectView components components added: ProjectList, ProjectImages, ProjectSelector also tried fixing an eslint error where it wasn't recognizing vue3 macros possibly wrong version of eslint installed. ref still not working

---
+++ src/components/project_view_components/ProjectImages.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+<template>
+    <v-card>
+        <div>TODO image format toggle</div>
+        <div>TODO Archive Image List</div>
+        <div>or</div>
+        <div>TODO Table Image List</div>
+    </v-card>
+</template>
