88d416e1b54620f5e90e453b9bb527fadab84d97
Carolina Capetillo <carolinacapetilloc@gmail.com>
2023-12-11 14:12:27 -0800

    added temporary display of images

---
+++ src/components/ImageCarousel.vue
@@ -0,0 +1,13 @@
+<script setup>
+import images from '../assets/images'
+
+</script>
+
+<template>
+    <li v-for="image in images" :key="image.id">
+        <img src={{ image }}>
+    </li>
+</template>
+
+<style scoped>
+</style>
