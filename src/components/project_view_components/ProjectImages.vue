65ebbaaab0065e98b7a5f384e06ff4c042419da1
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-12 13:07:50 -0800

    Removing props const and adding width css to view removed the props const as it wasn't correct practice for props in vue3 also added some width styling to the view so it has a more consistent look when projects are opened and closed

--- src/components/project_view_components/ProjectImages.vue
+++ src/components/project_view_components/ProjectImages.vue
@@ -1,11 +1,11 @@
 <script setup>
     // this prop should be enough to know which project to fetch images from
-    const props = defineProps(['selectedProject'])
+    defineProps(['selectedProject'])
 </script>
 <template>
     <v-card>
         <!-- These divs are just for demonstration purposes, feel free to remove them when implemented -->
-        <div>{{ props.selectedProject }}</div>
+        <div>{{ selectedProject }}</div>
         <div>under construction: Image format toggle</div>
         <div>under construction: Image Carousel or Archive List</div>
     </v-card>
