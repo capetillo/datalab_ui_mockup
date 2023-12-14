9c2d65d8783db57427c99a8ac4caeaf72209c3a9
Carolina Capetillo <carolinacapetilloc@gmail.com>
2023-12-13 16:07:16 -0800

    imported to projectimages

--- src/components/project_view_components/ProjectImages.vue
+++ src/components/project_view_components/ProjectImages.vue
@@ -1,4 +1,5 @@
 <script setup>
+import ImageCarousel from '../ImageCarousel.vue';
     // this prop should be enough to know which project to fetch images from
     defineProps(['selectedProject'])
 </script>
@@ -6,7 +7,6 @@
     <v-card>
         <!-- These divs are just for demonstration purposes, feel free to remove them when implemented -->
         <div>{{ selectedProject }}</div>
-        <div>under construction: Image format toggle</div>
-        <div>under construction: Image Carousel or Archive List</div>
+        <ImageCarousel></ImageCarousel>
     </v-card>
 </template>
