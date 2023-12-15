9038c36138ccc431d628dde5cffe95c3d6c6fce0
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-15 13:22:32 -0800

    Selection logic for parent ProjectImages component added logic for maintaining a list of selected images in the parent component so that if we change how images are displayed we will still have the same projects selected

--- src/components/project_view_components/ProjectImages.vue
+++ src/components/project_view_components/ProjectImages.vue
@@ -1,6 +1,14 @@
 <script setup>
+    import ImageList from './ImageList.vue';
     // this prop should be enough to know which project to fetch images from
     defineProps(['selectedProject'])
+
+    // functionality to switch between image layouts while retaining selected images
+    let selectedImages = []
+
+    function updateSelectedImages(selected){
+        selectedImages = selected
+    }
 </script>
 <template>
     <v-card>
@@ -8,5 +16,6 @@
         <div>{{ selectedProject }}</div>
         <div>under construction: Image format toggle</div>
         <div>under construction: Image Carousel or Archive List</div>
+        <image-list :selectedImages="selectedImages" @update-selected="updateSelectedImages"/>
     </v-card>
 </template>
