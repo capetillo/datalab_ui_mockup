a2fffe2d12f78fc6cbfabeb5647acbe0c875393e
Lloyd Dakin <taidakin99lol@gmail.com>
2023-12-15 13:22:32 -0800

    Selection logic for parent ProjectImages component added logic for maintaining a list of selected images in the parent component so that if we change how images are displayed we will still have the same projects selected

---
+++ src/components/project_view_components/ImageList.vue
@@ -0,0 +1,22 @@
+<script setup>
+    import { ref } from 'vue'
+    defineProps(['projectImageObjects', 'selectedImages'])
+
+    // v-data-table setup variables
+    let headers = ref([
+        { title: 'Image Name', align: 'start', sortable: true, key: 'imageName'},
+        { title: 'Time', align:'end', sortable: true, key: 'time'},
+        { title: 'Object', align:'end', sortable: true, key: 'object'}
+    ])
+    let itemsPerPage = ref(20)
+    let selected = ref([])
+</script>
+<template>
+    <v-data-table
+        :headers="headers"
+        v-model:items-per-page="itemsPerPage"
+        show-select
+        v-model="selected"
+        @update:selected="$emit('updateSelected', selected)"
+    ></v-data-table>
+</template>
