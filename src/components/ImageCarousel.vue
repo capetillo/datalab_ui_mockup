507df71ad625af3a608a7a52e5f06906d717f7b8
Carolina Capetillo <carolinacapetilloc@gmail.com>
2023-12-15 13:12:00 -0800

    fixed carousel display

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -6,12 +6,20 @@ import { Carousel, Slide, Navigation } from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
 
 const state = reactive({
-    currentSlide: 0
+    currentSlide: 0,
+    selectedImages: []
 })
 
-const slideTo = (index) => {
+const handleThumbnailClick = (item, index) => {
+
+
+    // Updates current slide index
     state.currentSlide = index
+    if (!state.selectedImages.find(img => img.basefile_name === item.basefile_name)) {
+        state.selectedImages.push(item)
+    }
 }
+
 
 // Refs for the carousels (if needed)
 // const mainCarousel = ref(null);
@@ -21,7 +29,7 @@ const slideTo = (index) => {
 
 <template>
     <!-- Main Carousel -->
-    <Carousel id="gallery" :items-to-show="1" :wrap-around="false" v-model="currentSlide">
+    <Carousel id="gallery" :items-to-show="1" :wrap-around="false" transition="0" v-model="state.currentSlide">
       <Slide v-for="(item, index) in MockData" :key="index">
         <div class="carousel__item">
           <img :src="require('@/assets/' + item.image)" class="div__item"/>
@@ -34,13 +42,13 @@ const slideTo = (index) => {
     id="thumbnails"
     :items-to-show="5"
     :wrap-around="false"
-    v-model="currentSlide"
+    v-model="state.currentSlide"
     ref="carousel"
   >
     <Slide v-for="(item, index) in MockData" :key="index">
-      <div class="thumbnail__item" @click="slideTo(index)">
+      <div class="thumbnail__item" @click="handleThumbnailClick(item, index)">
         <!-- Thumbnail Image -->
-        <img :src="require('@/assets/' + item.image)" class="thumbnail__item"/>
+        <img :src="require('@/assets/' + item.image)" :class="{'selected-thumbnail': currentSlide === index}" class="thumbnail__item"/>
       </div>
     </Slide>
     <template #addons>
@@ -60,5 +68,9 @@ const slideTo = (index) => {
   max-height: 160px;
   object-fit: cover;
   margin: 0 auto;
+}
+
+.selected-thumbnail {
+  border: 4px solid yellow;
 }
 </style>
