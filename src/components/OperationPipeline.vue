2ba6d150fe6d1f29e583dd9cbffc1a74c197b727
Jon <jnation@lco.global>
2023-12-21 21:15:20 -0100

    Add operation wizard to create operations

--- src/components/OperationPipeline.vue
+++ src/components/OperationPipeline.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { ref } from 'vue'
+import OperationWizard from './OperationWizard.vue';
+
+const emit = defineEmits(['addOperation'])
 
 defineProps({
   operations: {
@@ -9,6 +12,7 @@ defineProps({
 })
 
 const selectedOperation = ref(-1);
+const showWizardDialog = ref(false);
 
 function selectOperation(index) {
   if (index == selectedOperation.value) {
@@ -36,5 +40,9 @@ function operationBtnColor(index) {
       <v-btn :color="operationBtnColor(index)" variant="outlined" @click="selectOperation(index)">{{ index }}: {{ operation.name }}</v-btn>
   </v-row>
   <v-divider class="mb-4 mt-4"></v-divider>
-  <v-btn variant="flat" color="indigo-darken-2">Add Operation</v-btn>
+  <v-btn variant="flat" color="indigo-darken-2">Add Operation
+    <v-dialog v-model="showWizardDialog" activator="parent" fullscreen transition="dialog-bottom-transition">
+      <operation-wizard @close-wizard="showWizardDialog = false" @add-operation="emit('addOperation', $event); showWizardDialog = false;"></operation-wizard>
+    </v-dialog>
+  </v-btn>
 </template>
