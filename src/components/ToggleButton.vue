30060ba2dd5bfeff8b4d9cf52de070bfbecdf53c
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-02 14:00:06 -0800

    added toggle functionality

---
+++ src/components/ToggleButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import ProjectImages from './ProjectView/ProjectImages'
+import ProjectList from './ProjectView/ProjectList.vue'
+
+const isCarouselView = ref(true)
+
+const toggleView = () => {
+  isCarouselView.value = !isCarouselView.value
+  console.log("Toggled, new value:", isCarouselView.value)
+}
+
+</script>
+
+<template>
+    <v-btn @click="toggleView">
+    TOGGLE
+    </v-btn>
+   
+  <div v-if="isCarouselView">
+    <ProjectImages/>
+  </div>
+
+  <div v-else>
+    <ProjectList/>
+  </div>
+</template>
