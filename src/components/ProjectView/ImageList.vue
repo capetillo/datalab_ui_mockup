b082a9175f03507e5a86b8d5e62a3ecedfb59d45
Lloyd Dakin <taidakin99lol@gmail.com>
2024-01-04 14:32:12 -0800

    some styling changes and removing selectedimages from parent since we are using store now

--- src/components/ProjectView/ImageList.vue
+++ src/components/ProjectView/ImageList.vue
@@ -1,41 +1,46 @@
 <script setup>
-    import { ref } from 'vue'
-    const props = defineProps(['projectImageObjects', 'selectedImages', 'data'])
+import { ref } from "vue";
+import { useStore } from "vuex";
 
-    // v-data-table setup variables
-    let headers = ref([
-        { title: 'Image Name', align: 'start', sortable: true, key: 'basefile_name'},
-        { title: 'Time', align:'start', sortable: true, key: 'time'},
-        { title: 'Object', align:'start', sortable: true, key: 'object'},
-        { title: 'Image', align:'start', sortable: false, key: 'image'}
-    ])
-    let items = ref(props.data)
-    let itemsPerPage = ref(15)
-    let selected = ref([])
+const props = defineProps(["projectImageObjects", "selectedImages", "data"]);
+const store = useStore();
+
+// v-data-table setup variables
+let headers = ref([
+  { title: "Image Name", align: "start", sortable: true, key: "basefile_name" },
+  { title: "Time", align: "start", sortable: true, key: "time" },
+  { title: "Object", align: "start", sortable: true, key: "object" },
+  { title: "Image", align: "start", sortable: false, key: "image" },
+]);
+let items = ref(props.data);
+let itemsPerPage = ref(15);
+
+// selected images logic
+function selectRow(item) {
+  store.dispatch("toggleImageSelection", item);
+}
 </script>
 
 <template>
-    <v-data-table
-        :headers="headers"
-        :items="items"
-        v-model:items-per-page="itemsPerPage"
-        show-select
-        v-model="selected"
-        item-value="basefile_name"
-        density="compact"
-        @update:modelValue="$emit('updateSelected', selected)"
-    >
-        <!-- TODO change src to fetch image from url-->
-        <template #[`item.image`]="{ item }">
-            <v-img
-            :src="require('@/assets/' + item.image)"
-            :alt="item.basefile_name"
-            height="72"
-            width="128"
-            cover
-            >
-            </v-img>
-        </template>
-    </v-data-table>
-    
+  <v-data-table
+    :headers="headers"
+    :items="items"
+    @click="selectRow(item)"
+    v-model:items-per-page="itemsPerPage"
+    show-select
+    item-value="basefile_name"
+    density="compact"
+  >
+    <!-- TODO change src to fetch image from url-->
+    <template #[`item.image`]="{ item }">
+      <v-img
+        :src="require('@/assets/' + item.image)"
+        :alt="item.basefile_name"
+        height="72"
+        width="128"
+        cover
+      >
+      </v-img>
+    </template>
+  </v-data-table>
 </template>
--- src/components/ProjectView/ProjectImages.vue
+++ src/components/ProjectView/ProjectImages.vue
@@ -1,21 +1,14 @@
 <script setup>
-    import { ref } from 'vue'
-    import ImageList from './ImageList.vue'
-    // TODO for now we will just import project data from mock data but in the future call an api based on selectedProject
-    import MockData from '../../assets/MockData.JSON'
+import ImageList from "./ImageList.vue";
+// TODO for now we will just import project data from mock data but in the future call an api based on selectedProject
+import MockData from "../../assets/MockData.JSON";
 
-    // functionality to switch between image layouts while retaining selected images
-    defineProps(['selectedProject'])
-
-    let selectedImages = ref([])
-
-    function updateSelectedImages(selected){
-        selectedImages.value = selected
-    }
+// functionality to switch between image layouts while retaining selected images
+defineProps(["selectedProject"]);
 </script>
 
 <template>
-        <!-- These divs are just for demonstration purposes, feel free to remove them when implemented -->
-    <div>{{ selectedProject }}</div>
-    <image-list :data="MockData" :selectedImages="selectedImages" @update-selected="updateSelectedImages"/>
+  <!-- These divs are just for demonstration purposes, feel free to remove them when implemented -->
+  <div>{{ selectedProject }}</div>
+  <image-list :data="MockData" />
 </template>
