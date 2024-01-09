78dfb6efd23488a8da3695e4eb4b36f79f47e924
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-09 08:40:02 -0800

    added logic to add datasessions to a local array

--- src/components/ImportButton.vue
+++ src/components/ImportButton.vue
@@ -1,11 +1,43 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import { ref } from 'vue'
 
 const router = useRouter()
 const store = useStore()
+const dataSessions = ref([])
+
+
+async function getDataSession() {
+  try {
+    const response = await fetch ('http://127.0.0.1:8000/api/datasessions/', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Token 123456789abcdefg',
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    })
+    if (!response.ok) {
+      const errorData = await response.json()
+      console.error('Error Response Data:', errorData)
+      throw new Error('Bad request')
+    }
+    const data = await response.json()
+    const results = data.results
+    for (const result of results) {
+      const dataSession = result.name
+      if (!dataSessions.value.includes(dataSession)) {
+        dataSessions.value.push(dataSession)
+      }
+    }
+  } catch (error) {
+    console.log('Error getting images: ', error)
+  }
+}
+
 
 const importImages = async () => {
+  getDataSession()
   try {
     const selectedImages = store.state.selectedImages
     const inputData = selectedImages.map(image => ({
