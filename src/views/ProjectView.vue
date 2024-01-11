b070513c1e2c36fbb63078bf04e5abb26be62503
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-10 16:16:07 -0800

    added functionality so that 1. user can create new data session name 2. post to that new data session 3. if the data session name exists, it doesnt post to the api and it warns the user that the name already exists 4. redirects user to datasessions view after new data session is created

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -9,6 +9,8 @@ const router = useRouter()
 const store = useStore()
 const isPopupVisible = ref(false)
 const uniqueDataSessions = ref([])
+const newSessionName = ref('')
+const errorMessage = ref('')
 
 const getDataSessions = async () => {
   try {
@@ -59,6 +61,43 @@ const selectDataSession = (session) => {
   addImagesToExistingSession(session)
   router.push({ name: 'DataSessions' })
 }
+
+const createNewDataSession = async () => {
+    if (doesSessionNameExist(newSessionName.value)) {
+        errorMessage.value = 'Data Session name already exists. Please choose a different name.'
+        return
+    }
+
+    try {
+        const selectedImages = store.state.selectedImages
+        const inputData = selectedImages.map(image => ({
+            'source': image.image,
+            'basename': image.basefile_name
+        }))
+        const requestBody = { 
+            'name': newSessionName.value,
+            'input_data': inputData 
+        }
+        const url = 'http://127.0.0.1:8000/api/datasessions/'
+        const data = await fetchApiCall(url, 'POST', requestBody)
+
+        console.log('New Session Created:', data)
+
+        isPopupVisible.value = false
+        newSessionName.value = ''
+        errorMessage.value = ''
+
+        router.push({ name: 'DataSessions' })
+    } catch (error) {
+        console.log('Error creating new data session:', error)
+        errorMessage.value = 'Error creating new data session'
+    }
+}
+
+const doesSessionNameExist = (name) => {
+    return uniqueDataSessions.value.some(session => session.name === name)
+}
+
 </script>
 
 <template>
@@ -66,21 +105,26 @@ const selectDataSession = (session) => {
     <v-btn @click="getDataSessions">Add to a Session</v-btn>
     
     <v-dialog v-model="isPopupVisible" width="300">
-      <v-card>
-        <v-card-title>Data Sessions</v-card-title>
-        <v-card-text>
-          <v-list>
-            <v-list-item v-for="session in uniqueDataSessions" :key="session.id" @click="selectDataSession(session)">
-              <v-list-item-content>
-                {{ session.name }}
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" text @click="isPopupVisible = false">Close</v-btn>
-        </v-card-actions>
-      </v-card>
+        <v-card>
+            <v-card-title>Data Sessions</v-card-title>
+            <v-card-text>
+            <v-list>
+                <v-list-item v-for="session in uniqueDataSessions" :key="session.id" @click="selectDataSession(session)">
+                <v-list-item-content>
+                    {{ session.name }}
+                </v-list-item-content>
+                </v-list-item>
+            </v-list>
+            <!-- Input for new session name -->
+            <v-text-field v-model="newSessionName" label="New Session Name" />
+            <!-- Error message -->
+            <div v-if="errorMessage">{{ errorMessage }}</div>
+            </v-card-text>
+            <v-card-actions>
+            <v-btn color="primary" text @click="createNewDataSession">Create New Session</v-btn>
+            <v-btn color="primary" text @click="isPopupVisible = false">Close</v-btn>
+            </v-card-actions>
+        </v-card>
     </v-dialog>
   </template>
   
