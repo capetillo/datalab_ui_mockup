14aac7ad149b870787b4177dca1d6fa5c7de73ce
Lloyd Dakin <taidakin99lol@gmail.com>
2024-01-11 14:57:11 -0800

    changed css styling to use grid for more control, also removed old selected ref that was unused

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -13,15 +13,42 @@
     let imageDisplayToggle = ref(true)
 </script>
 <template>
-    <v-container fluid>
-        <div class="d-flex">
-            <ProjectBar class="w-25"/>
-            <div class="w-75">
-                <ImageCarousel v-if="imageDisplayToggle" :data="MockData"/>
-                <ImageList class="overflow-auto" v-if="!imageDisplayToggle" :data="MockData"/>
-                <v-switch class="d-flex ml-auto" v-model="imageDisplayToggle" inset prepend-icon="mdi-view-list" append-icon="mdi-image"/>
+    <div class="container">
+        <ProjectBar class="project-bar"/>
+        <div class="image-area">
+            <ImageCarousel v-if="imageDisplayToggle" :data="MockData"/>
+            <ImageList v-if="!imageDisplayToggle" :data="MockData"/>
+            <div class="control-buttons">
+                <v-switch class="d-flex mr-4" v-model="imageDisplayToggle" inset prepend-icon="mdi-view-list" append-icon="mdi-image"/>
+                <ImportButton/>
             </div>
         </div>
-        <ImportButton/>
-    </v-container>
+    </div>
 </template>
+<style scoped>
+.container{
+    margin: 20px;
+    display: grid;
+    grid-template-columns: [col1-start] 1fr [col1-end col2-start] 80% [col2-end];
+    grid-template-rows: [row-start] 100% [row-end];
+    justify-content: space-between;
+}
+.project-bar{
+    display: flex;
+    grid-column-start: col1-start;
+    grid-column-end: col1-end;
+    grid-row-start: row-start;
+    grid-row-end: row-end;
+}
+.image-area{
+    grid-column-start: col2-start;
+    grid-column-end: col2-end;
+    margin-left: 10px;
+}
+.control-buttons{
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+    float: right;
+}
+</style>
