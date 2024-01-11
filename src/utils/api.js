ecae569bc9288ca74d401088093d4c6885810685
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-11 08:13:39 -0800

    Rewrote fetchapi call. Now instead of the whole function being in a try catch block, only the relevant parts are

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,30 @@
 async function fetchApiCall(url, method, body = null) {
-    try {
-      const headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': 'Token 123456789abcdefg',
-      }
-  
-      const config = {
-        method: method,
-        headers: headers,
-        body: body ? JSON.stringify(body) : null
-      }
-
-      const response = await fetch(url, config)
-  
-      if (!response.ok) {
-        try {
-          const errorData = await response.json()
-          throw new Error(errorData.message || 'Error fetching data')
-        } catch (jsonError) {
-          throw new Error('Error fetching data')
-        }
-      }
-  
-      return await response.json()
-    } catch (error) {
-      console.error('Fetch API Call Error:', error)
-      throw error
-    }
+  const headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': 'Token 123456789abcdefg',
   }
+  
+  const config = {
+    method: method,
+    headers: headers,
+    body: body ? JSON.stringify(body) : null
+  }
+
+  try {
+    const response = await fetch(url, config)
+    if (!response.ok) {
+      console.error("Response not OK", response)
+      throw Error("Response not OK", response)
+    }
+    else {
+      return await response.json()
+    }
+  } catch(error) {
+    console.error("Error raised when sending request.", error)
+    throw Error("Error raised when sending request.", error)
+  }
+}
   
   export { fetchApiCall }
   
