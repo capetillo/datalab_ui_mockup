3e7f47dde08ffb9fcef7422e7510c388738899d9
Lloyd Dakin <taidakin99lol@gmail.com>
2024-01-18 13:39:17 -0800

    Datasession Dialog Component and api pulled the dialog out into a reusable component, added an api call to the backend and deleting images

---
+++ src/components/DeleteSessionDialog.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { fetchApiCall } from '@/utils/api';
+const props = defineProps([ 'modelValue', 'deleteId'])
+const emit = defineEmits(['update:modelValue', 'refreshDataSessions'])
+
+function closeDialog() { emit('update:modelValue', false)}
+
+async function confirmDeleteSession() {
+  if (props.deleteId != -1) {
+    console.log('deleting session with id ' + props.deleteId)
+    const url = 'http://127.0.0.1:8000/api/datasessions/'+props.deleteId+'/'
+    try {
+      await fetchApiCall(url, 'DELETE' )
+      .then(response => response.json())
+    } catch (error) {
+      console.log('error calling api: ', error)
+    }
+    emit('refreshDataSessions')
+    closeDialog()
+  }
+}
+</script>
+<template>
+  <!-- Shared dialog used to confirm deleting of sessions -->
+  <v-dialog
+    :model-value="modelValue"
+    persistent
+    width="auto"
+  >
+    <v-card>
+      <v-card-title class="text-h5" color="red-lighten-1">
+        Delete Session
+      </v-card-title>
+      <v-card-text>Are you sure you want to delete this Datalab Session? This operation is not reversible!</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="green-darken-1"
+          variant="text"
+          @click="closeDialog()"
+        >
+          Go Back
+        </v-btn>
+        <v-btn
+          color="red-darken-1"
+          variant="text"
+          @click="confirmDeleteSession()"
+        >
+          Delete
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
