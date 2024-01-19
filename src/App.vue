8f1c16203d3f7722d1c8d725f2dd975dd86dfde3
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-19 15:21:08 -0800

    moved loadconfig to app.vue

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,23 @@ import { loadConfig } from './utils/api'
 const store = useStore()
 const loadedConfig = computed(() => store.state.isConfigLoaded)
 
+
+// loads config data
+async function loadConfiguration() {
+  try {
+    const response = await fetch('/config/config.json')
+    if (!response.ok) {
+      throw Error('Failed to load configuration')
+    }
+    const configData = response.json()
+    return configData
+  } catch (error) {
+    console.error('Error loading configuration:', error)
+  }
+}
 // invoking loadConfig when app first mounts
 onMounted(async () => {
-  const config = await loadConfig()
+  const config = await loadConfiguration()
 
   // only render if config is loaded
   if (config) {
