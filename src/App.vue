2d01cdd53ca6635a7e2e236cb7fc9c391b13d7f3
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-19 14:10:23 -0800

    added loadedconfig and only renders if we get config data. then we commit to state and set configloaded to true (in order to render), setdatasessionsurl to the url in the config file and same for archive url

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+import { onMounted, computed } from 'vue'
+import { useStore } from 'vuex'
+import { loadConfig } from './utils/api'
+
+const store = useStore()
+const loadedConfig = computed(() => store.state.isConfigLoaded)
+
+// invoking loadConfig when app first mounts
+onMounted(async () => {
+  const config = await loadConfig()
+
+  // only render if config is loaded
+  if (config) {
+    store.commit('setIsConfigLoaded', true)
+    store.commit('setDataSessionsUrl', config.dataSessionsUrl)
+    store.commit('setArchiveUrl', config.dataLabArchiveUrl)
+  }  
+})
 </script>
 
 <template>
-  <div>
+  <div v-if="loadedConfig">
+    <div>hello world</div>
     <router-view></router-view>
   </div>
 </template>
