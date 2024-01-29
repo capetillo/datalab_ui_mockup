ad25eb90be39ef25b1a5b292f5d0f07cf39b4aba
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-29 15:17:49 -0800

    updated for eslint. everything works as it used to

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,34 +1,34 @@
 // handles api requests for datasessions with configurable parameters and callback functions
 async function fetchApiCall({ url, method, body = null, headers, successCallback = null, failCallback = null }) {
-  const config = {
-    method: method,
-    headers: headers,
-    body: body ? JSON.stringify(body) : null
-  }
+	const config = {
+		method: method,
+		headers: headers,
+		body: body ? JSON.stringify(body) : null
+	}
 
-  try {
-    const response = await fetch(url, config)
+	try {
+		const response = await fetch(url, config)
 
-    // Check for empty or non-JSON response
-    if (response.ok && response.headers.get('Content-Length') === '0') {
-      // No content return a success with null
-      successCallback ? successCallback(null) : null
-    } else {
-      const responseData = await response.json()
+		// Check for empty or non-JSON response
+		if (response.ok && response.headers.get('Content-Length') === '0') {
+			// No content return a success with null
+			successCallback ? successCallback(null) : null
+		} else {
+			const responseData = await response.json()
 
-      if (!response.ok) {
-        console.error('Response not OK', responseData)
-        failCallback ? failCallback(responseData, response.status) : null
-      } else {
-        // Invoking success callback with responseData
-        successCallback ? successCallback(responseData) : null
-      }
-      return responseData
-    }
-  } catch (error) {
-    console.error('Error raised when sending request', error)
-    failCallback ? failCallback(error) : null
-  }
+			if (!response.ok) {
+				console.error('Response not OK', responseData)
+				failCallback ? failCallback(responseData, response.status) : null
+			} else {
+				// Invoking success callback with responseData
+				successCallback ? successCallback(responseData) : null
+			}
+			return responseData
+		}
+	} catch (error) {
+		console.error('Error raised when sending request', error)
+		failCallback ? failCallback(error) : null
+	}
 }
 
 export { fetchApiCall }
