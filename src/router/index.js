ff099989e202180f6de18fca05c2d27ca51ade92
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-29 15:17:21 -0800

    updated file to fit eslint rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 import DataSessions from '../views/DataSessionsView.vue'
 import ProjectView from '../views/ProjectView.vue'
 import Home from '../views/HomeView.vue'
 import Registration from '../views/RegistrationView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-      path: '/projects',
-      name: 'ProjectView',
-      component: ProjectView
-  },
-  {
-    path: '/datasessions/',
-    name: 'DataSessions',
-    component: DataSessions,
-  },
-  {
-    path: '/register/',
-    name: 'Registration',
-    component: Registration,
-  }
-];
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/projects',
+		name: 'ProjectView',
+		component: ProjectView
+	},
+	{
+		path: '/datasessions/',
+		name: 'DataSessions',
+		component: DataSessions,
+	},
+	{
+		path: '/register/',
+		name: 'Registration',
+		component: Registration,
+	}
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
-});
+	history: createWebHistory(process.env.BASE_URL),
+	routes
+})
 
-export default router;
+export default router
