30fc1c599dbc0cd89096ca2c000f9d8278c8a420
Lloyd Dakin <taidakin99lol@gmail.com>
2024-01-30 13:44:29 -0800

    removed copied reload session emit

--- src/components/DataSession.vue
+++ src/components/DataSession.vue
@@ -29,7 +29,7 @@ async function addOperation(operationDefinition) {
 
 const getImages = async () => {
 	const url = dataSessionsUrl + props.data.id
-	const data = await fetchApiCall({url: url, method: 'GET', headers: authHeaders, successCallback: emit('reloadSession'), failCallback: handleError})
+	const data = await fetchApiCall({url: url, method: 'GET', headers: authHeaders, failCallback: handleError})
 	images.value = data.input_data
 }
 
