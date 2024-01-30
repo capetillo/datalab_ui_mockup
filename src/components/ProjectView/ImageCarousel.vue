5dd4eb2afe3fd3980bd8520cee3f86bd9c6bfefa
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-01-30 12:47:14 -0800

    deleted comments

--- src/components/ProjectView/ImageCarousel.vue
+++ src/components/ProjectView/ImageCarousel.vue
@@ -16,23 +16,14 @@ const isSelected = (item) => store.getters.isSelected(item)
 
 // Invoked any time an image is clicked
 const handleThumbnailClick = (item, index) => {
-	// Dispatch an action to toggle the selection status of the clicked image
-	// This will add the image to the selectedImages array in the store if it's not there
-	// or remove it if it is already in the array
 	store.dispatch('toggleImageSelection', item)
 
 	// Checking if there are any selected images after the toggle action
 	if (store.state.selectedImages.length > 0) {
-		// If there are selected images, then we find the last selected image
 		const lastSelectedImage = store.state.selectedImages[store.state.selectedImages.length - 1]
-		// Then we find the index of the last selected image in the data array
-		// This is used to set the current slide to the last selected image
 		const lastSelectedIndex = data.value.findIndex(img => img.basename === lastSelectedImage.basename)
-		// Set the current slide to the last selected image
 		currentSlide.value = lastSelectedIndex
 	} else {
-		// If no images are selected, set the current slide to the index of the clicked image
-		// This ensures that the carousel shows the clicked image
 		currentSlide.value = index
 	}
 }
