5eea369789260754f4eec2ce291144c652e3fb39
Lloyd Dakin <taidakin99lol@gmail.com>
2024-02-06 16:11:54 -0800

    login page fixes, css, and default page

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { fetchApiCall } from '@/utils/api'
+import lambdaLogo from '../assets/PTR-lambda.png'
 
 const store = useStore()
 const router = useRouter()
@@ -30,78 +31,95 @@ const handleError = (error) => {
 	errorMessage.value = 'Failed to authenticate user'
 }
 
-const authenticateUser = async () => {
-	const requestBody = {
-		username: username.value,
-		password: password.value
-	}
-
-	const authHeaders = {
-		'Content-Type': 'application/json',
-		'Accept': 'application/json',
-	}
-
-	try {
-		await fetchApiCall({ url: store.state.observationPortalUrl + 'api-token-auth/', method: 'POST', body: requestBody, headers: authHeaders, successCallback: storeToken, failCallback: handleError })
-	} catch (error) {
-		handleError(error)
-	}
-}
-
 const storeUser = (user) => {
 	store.dispatch('userData/setUsername', username.value)
 	store.dispatch('userData/setUserProfile', user)
 	store.commit('setProjects', user.proposals)
-	router.push({ name: 'ProjectView' })
+	router.push({ name: 'DataSessions' })
 }
 
-const getUserProfile = async () => {
-	await authenticateUser ()
-	try {
-		await fetchApiCall({ url: store.state.observationPortalUrl + 'profile/', method: 'GET', successCallback: storeUser, failCallback: handleError })
-	} catch (error) {
-		handleError(error)
+const Login = async () => {
+	const requestBody = {
+		username: username.value,
+		password: password.value
 	}
+	// store an auth token from login credentials
+	await fetchApiCall({ url: store.state.observationPortalUrl + 'api-token-auth/', method: 'POST', body: requestBody, successCallback: storeToken, failCallback: handleError })
+	// store user profile using auth token
+	await fetchApiCall({ url: store.state.observationPortalUrl + 'profile/', method: 'GET', successCallback: storeUser, failCallback: handleError })
 }
 
 </script>
 
 <template>
-  <v-container>
-    <v-form @submit.prevent="getUserProfile">
-      <v-text-field
-        v-model="username"
-        label="Username"
-        :rules="[rules.required]"
-        required
-      />
-
-      <v-text-field
-        v-model="password"
-        label="Password"
-        :type="showPassword ? 'text' : 'password'"
-        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        :rules="[rules.required]"
-        required
-        @click:append="showPassword = !showPassword"
-      />
-      <v-btn
-        type="submit"
-        color="primary"
-      >
-        Login
-      </v-btn>
-      <div
-        v-if="errorMessage"
-        class="error-message"
-      >
-        {{ errorMessage }}
+  <v-container class="registration-container">
+    <v-card class="login-card pa-10">
+      <div class="login-title">
+        <img
+          class="lambdaLogo"
+          :src="lambdaLogo"
+        >
+        <h1>Datalab</h1>
       </div>
-    </v-form>
+      <v-form
+        class="pt-7"
+        @submit.prevent="Login"
+      >
+        <v-text-field
+          v-model="username"
+          autocomplete="username"
+          label="Username"
+          :rules="[rules.required]"
+          required
+        />
+
+        <v-text-field
+          v-model="password"
+          autocomplete="current-password"
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required]"
+          required
+          @click:append="showPassword = !showPassword"
+        />
+        <v-btn
+          type="submit"
+          color="primary"
+        >
+          Login
+        </v-btn>
+        <div
+          v-if="errorMessage"
+          class="error-message"
+        >
+          {{ errorMessage }}
+        </div>
+      </v-form>
+    </v-card>
   </v-container>
 </template>
 
 <style scoped>
+.registration-container{
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+
+}
+.login-card{
+  width: 500px;
+}
+.lambdaLogo{
+  height: 1.8em;
+  margin-right: 10px;
+}
+.login-title{
+  display: flex;
+  align-items: center;
+}
 .error-message {
     color: red;
     margin-top: 10px;
