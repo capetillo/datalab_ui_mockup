991415ab4595a7a0004268c54331c0214c17372f
Lloyd Dakin <taidakin99lol@gmail.com>
2024-02-09 12:42:21 -0800

    base image analyzer component

---
+++ src/components/ImageAnalyzer.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps(['modelValue', 'image'])
+const emit = defineEmits(['update:modelValue'])
+
+function closeDialog() { 
+	console.log('image prop:', props.image)
+	emit('update:modelValue', false)
+}
+
+</script>
+<template>
+  <v-dialog
+    :model-value="modelValue"
+    fullscreen
+  >
+    <v-card>
+      <v-img
+        :src="hello"
+        cover
+      />
+      <v-btn
+        color="green-darken-1"
+        variant="text"
+        @click="closeDialog()"
+      >
+        Close
+      </v-btn>
+    </v-card>
+  </v-dialog>
+</template>
