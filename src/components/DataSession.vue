4e2860350e9c91f93736c6dabad47c95d831779c
Lloyd Dakin <taidakin99lol@gmail.com>
2024-02-21 16:21:24 -0800

    add image Anaylzer to data sessions

--- src/components/DataSession.vue
+++ src/components/DataSession.vue
@@ -1,12 +1,15 @@
 <script setup>
 import { ref, onMounted, defineEmits, defineProps } from 'vue'
 import OperationPipeline from './OperationPipeline.vue'
+import ImageAnalyzer from './ImageAnalyzer.vue'
 import { fetchApiCall, handleError } from '../utils/api'
 import { useStore } from 'vuex'
 
 const store = useStore()
 const emit = defineEmits(['reloadSession'])
 let images = ref([])
+const showAnalysisDialog = ref(false)
+const analysisImage = ref()
 const dataSessionsUrl = store.state.datalabApiBaseUrl + 'datasessions/'
 
 const props = defineProps({
@@ -26,6 +29,11 @@ async function addOperation(operationDefinition) {
 		}
 	}
 	await fetchApiCall({url: url, method: 'POST', body: operationDefinition, successCallback: emit('reloadSession'), failCallback: handleError})
+}
+
+const analyze = (imageBasename) => {
+	analysisImage.value = store.getters.largeImage(imageBasename)
+	showAnalysisDialog.value = true
 }
 
 const saveImages = (data) => {
@@ -70,6 +78,7 @@ onMounted(() => {
           :alt="image.basename"
           cover
           aspect-ratio="1"
+          @click="analyze(image.basename)"
         />
       </v-col>
     </v-row>
@@ -85,6 +94,10 @@ onMounted(() => {
       />
     </v-col>
   </v-container>
+  <image-analyzer
+    v-model="showAnalysisDialog"
+    :image="analysisImage"
+  />
 </template>
 
 <style scoped lang="scss">
