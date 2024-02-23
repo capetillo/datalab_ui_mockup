1557d8a18089be22ccff4debb00480384ddd7bea
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-02-23 12:17:59 -0800

    created the global component imagegrid

---
+++ src/components/Global/ImageGrid.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { defineProps, onMounted, ref } from 'vue'
+import { fetchApiCall, handleError } from '../../utils/api'
+
+const props = defineProps({
+	data: {
+		type: Object,
+		required: true
+	},
+	columnSpan: {
+		type: Number,
+		required: true
+	}
+})
+
+let images = ref([])
+
+const saveImages = (data) => {
+	const results = data.results
+	if (results.length) {
+		images.value.push(data.results[0])
+	}
+}
+
+const getImages = async () => {
+	const responseData = props.data
+	const inputData = responseData.input_data
+	for (const data of inputData) {
+		const basename = data.basename
+		const url =  `https://datalab-archive.photonranch.org/frames/?basename_exact=${basename}-small`
+		await fetchApiCall({url: url, method: 'GET', successCallback: saveImages, failCallback: handleError})
+	}
+}
+
+onMounted(() => {
+	getImages()
+})
+
+</script>
+
+<template>
+  <v-row v-if="images.length">
+    <v-col
+      v-for="image of images"
+      :key="image.basename"
+      :cols="columnSpan"
+    >
+      <v-img
+        :src="image.url"
+        :alt="image.basename"
+        cover
+        aspect-ratio="1"
+      />
+    </v-col>
+  </v-row>
+</template>
