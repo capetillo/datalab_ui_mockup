b7da4cc9a53aceb3e4873fb489c22914831ea897
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-02-27 12:09:28 -0800

    added colorblind toggle component with simple css

---
+++ src/components/ColorblindToggle.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const isColorblindMode = ref(false)
+
+function toggleColorblindMode() {
+	isColorblindMode.value = !isColorblindMode.value
+}
+
+</script>
+
+<template>
+  <div :class="{ 'colorblind-mode': isColorblindMode }">
+    <button @click="toggleColorblindMode">
+      {{ isColorblindMode ? 'Disable' : 'Enable' }} Colorblind Mode
+    </button>
+  </div>
+</template>
+
+<style>
+.colorblind-mode {
+	filter: invert(1)
+}
+</style>
