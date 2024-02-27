ffdcc71df487e720fa497ce6d7042c8e7ff56ead
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-02-27 14:31:53 -0800

    styled colorblindtoggle

--- src/components/ColorblindToggle.vue
+++ src/components/ColorblindToggle.vue
@@ -13,9 +13,27 @@ function toggleColorblindMode() {
 </script>
 
 <template>
-  <v-switch
-    v-model="isColorblindMode"
-    :label="`Colorblind Mode: ${isColorblindMode ? 'On' : 'Off'}`"
-    @click="toggleColorblindMode"
-  />
+  <div class="colorblind-toggle-container">
+    <v-switch
+      v-model="isColorblindMode"
+      :label="`Colorblind Mode: ${isColorblindMode ? 'On' : 'Off'}`"
+      class="colorblind-toggle"
+      @click="toggleColorblindMode"
+    />
+  </div>
 </template>
+
+<style scoped>
+.colorblind-toggle-container {
+  position: absolute;
+  right: 0.7rem;
+  top: -0.5rem;
+  z-index: 2000;
+  margin-right: 0.3rem;
+}
+.colorblind-toggle {
+  color: white;
+  font-weight: 600;
+  padding: 0.6rem;
+}
+</style>
