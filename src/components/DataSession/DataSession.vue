42983a3349a71227eefe026cab0da2f41de3c569
Jon <jnation@lco.global>
2024-03-05 07:02:38 -0100

    Updated the ImageGrid and DataSession View to show output images

--- src/components/DataSession/DataSession.vue
+++ src/components/DataSession/DataSession.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineEmits, defineProps } from 'vue'
+import { ref, defineEmits, defineProps, onMounted, watch } from 'vue'
 import OperationPipeline from './OperationPipeline.vue'
 import { fetchApiCall, handleError } from '../../utils/api'
 import { calculateColumnSpan } from '../../utils/common'
@@ -15,52 +15,107 @@ const props = defineProps({
 
 const store = useStore()
 const emit = defineEmits(['reloadSession'])
+const images = ref([])
+const filteredImages = ref([])
 
 const dataSessionsUrl = store.state.datalabApiBaseUrl + 'datasessions/'
 const imagesPerRow = 4
+const operationMap = {}
+var selectedOperation = -1
+
+async function addCompletedOperationsOutput() {
+	const url = dataSessionsUrl + props.data.id + '/operations/'
+
+	function updateCompletedOperations(response) {
+		response.results.forEach(operationResponse => {
+			if (operationResponse.status == 'COMPLETED') {
+				addCompletedOperation(operationResponse)
+			}
+		})
+	}
+
+	await fetchApiCall({ url: url, method: 'GET', successCallback: updateCompletedOperations, failCallback: handleError })
+}
+
+
+function imagesContainsFile(file) {
+	return images.value.some(image => image.basename == file.basename && image.source == file.source && image.operation == file.operation)
+}
+
+
+function addCompletedOperation(operationResponse) {
+	if ('output' in operationResponse && 'output_files' in operationResponse.output) {
+		operationResponse.output.output_files.forEach(outputFile => {
+			outputFile.operation = operationResponse.id
+			outputFile.operationIndex = operationMap[operationResponse.id]
+			if (!imagesContainsFile(outputFile)) {
+				images.value.push(outputFile)
+				if (selectedOperation == -1 || selectedOperation == outputFile.operationIndex) {
+					filteredImages.value.push(outputFile)
+				}
+			}
+		})
+	}
+}
+
+
+function selectOperation(operationIndex) {
+	if (operationIndex != selectedOperation){
+		selectedOperation = operationIndex
+		if (selectedOperation == -1){
+			filteredImages.value = images.value
+		}
+		else {
+			filteredImages.value = images.value.filter(image => image.operationIndex == operationIndex)
+		}
+	}
+}
 
 async function addOperation(operationDefinition) {
 	const url = dataSessionsUrl + props.data.id + '/operations/'
-	if ('input_files' in operationDefinition.input_data){
+	if ('input_files' in operationDefinition.input_data) {
 		for (const image of operationDefinition.input_data.input_files) {
 			image.source = 'archive'
 		}
 	}
 
 	// first operation doesn't load unless this is here
-	function postOperationSuccess(){
+	function postOperationSuccess() {
 		emit('reloadSession')
 	}
-	
-	await fetchApiCall({url: url, method: 'POST', body: operationDefinition, successCallback: postOperationSuccess, failCallback: handleError})
+
+	await fetchApiCall({ url: url, method: 'POST', body: operationDefinition, successCallback: postOperationSuccess, failCallback: handleError })
 }
+
+watch(() => props.data, () => {
+	props.data.operations.forEach((operation, index) => {
+		operationMap[operation.id] = index
+	})
+}, { immediate: true })
+
+
+onMounted(() => {
+	images.value = [...props.data.input_data]
+	filteredImages.value = [...images.value]
+	addCompletedOperationsOutput()
+})
 
 </script>
 
 <template>
-  <v-container class="d-lg-flex ds-container">
-    <image-grid
-      :data="data"
-      :column-span="calculateColumnSpan(data.input_data.length, imagesPerRow)"
-    />
-    <v-col
-      cols="3"
-      justify="center"
-      align="center"
-    >
-      <!-- The operations bar list goes here -->
-      <operation-pipeline
-        :data="data"
-        :operations="data.operations"
-        @add-operation="addOperation"
-      />
-    </v-col>
-  </v-container>
+	<v-container class="d-lg-flex ds-container">
+		<image-grid :images="filteredImages" :column-span="calculateColumnSpan(data.input_data.length, imagesPerRow)" />
+		<v-col cols="3" justify="center" align="center">
+			<!-- The operations bar list goes here -->
+			<operation-pipeline :images="images" :session_id="data.id" :operations="data.operations"
+				@add-operation="addOperation" @operation-completed="addCompletedOperation" @select-operation="selectOperation"/>
+		</v-col>
+	</v-container>
 </template>
 
 <style scoped>
 .ds-container {
-  background-color: var(--metal);
-  display:flex;
+	background-color: var(--metal);
+	display: flex;
 }
 </style>
--- src/components/DataSession/OperationWizard.vue
+++ src/components/DataSession/OperationWizard.vue
@@ -1,13 +1,13 @@
 <script setup>
-import { ref, onMounted, onUnmounted, computed, defineEmits, defineProps} from 'vue'
+import { ref, onMounted, onUnmounted, computed, defineEmits, defineProps } from 'vue'
 import { fetchApiCall, handleError } from '../../utils/api'
 import { calculateColumnSpan } from '../../utils/common'
 import ImageGrid from '../Global/ImageGrid'
 import { useStore } from 'vuex'
 
-defineProps({
-	data: {
-		type: Object,
+const props = defineProps({
+	images: {
+		type: Array,
 		required: true
 	}
 })
@@ -19,7 +19,7 @@ const dataSessionsUrl = store.state.datalabApiBaseUrl
 const availableOperations = ref({})
 const selectedOperation = ref('')
 const selectedOperationInput = ref({})
-const selectedDataSessionImages = ref([])
+const selectedImages = ref({})
 const imagesPerRow = ref(3)
 
 const updateImagesPerRow = () => {
@@ -37,8 +37,8 @@ let displayImages = ref(false)
 
 onMounted(async () => {
 	const url = dataSessionsUrl + 'available_operations/'
-	await fetchApiCall({url: url, method: 'GET', successCallback: (data) => {availableOperations.value = data}, failCallback: handleError})
-	if (Object.keys(availableOperations.value).length > 0){
+	await fetchApiCall({ url: url, method: 'GET', successCallback: (data) => { availableOperations.value = data }, failCallback: handleError })
+	if (Object.keys(availableOperations.value).length > 0) {
 		selectOperation(Object.keys(availableOperations.value)[0])
 	}
 	updateImagesPerRow()
@@ -54,12 +54,16 @@ const page = ref('select')
 function selectOperation(name) {
 	selectedOperation.value = name
 	selectedOperationInput.value = {}
+	selectedImages.value = {}
 	for (const [key, value] of Object.entries(selectedOperationInputs.value)) {
-		if ('default' in value){
+		if ('default' in value) {
 			selectedOperationInput.value[key] = value.default
 		}
 		else {
 			selectedOperationInput.value[key] = null
+		}
+		if (value.type == 'file') {
+			selectedImages.value[key] = []
 		}
 	}
 }
@@ -116,220 +120,216 @@ function goForward() {
 		let operationDefinition = {
 			'name': selectedOperation.value,
 			'input_data': {
-				...selectedOperationInput.value,
-				'input_files': selectedDataSessionImages.value
+				...selectedOperationInput.value
 			}
+		}
+		for (const inputKey in selectedImages.value) {
+			let selected = []
+			selectedImages.value[inputKey].forEach(index => {
+				selected.push(props.images[index])
+			})
+			operationDefinition.input_data[inputKey] = selected
 		}
 		emit('addOperation', operationDefinition)
 	}
 }
 
-const handleThumbnailClick = (item) => {
-	const index = selectedDataSessionImages.value.findIndex(selectedImage => selectedImage.basename === item.basename)
-	if (index === -1) {
-		selectedDataSessionImages.value.push(item)
-	} else {
-		selectedDataSessionImages.value.splice(index, 1)
+function selectImage(inputKey, imageIndex) {
+	if (selectedImages.value[inputKey].includes(imageIndex)) {
+		selectedImages.value[inputKey].splice(selectedImages.value[inputKey].indexOf(imageIndex), 1)
+	}
+	else {
+		selectedImages.value[inputKey].push(imageIndex)
 	}
 }
+
+// const handleThumbnailClick = (basename, item) => {
+// 	const index = selectedDataSessionImages.value.findIndex(selectedImage => selectedImage.basename === basename)
+// 	if (index === -1) {
+// 		selectedDataSessionImages.value.push(item)
+// 	} else {
+// 		selectedDataSessionImages.value.splice(index, 1)
+// 	}
+// }
 
 </script>
 
 <template>
-  <v-card class="wizard-background">
-    <v-toolbar class="wizard-toolbar">
-      <v-toolbar-title class="wizard-title">
-        {{ wizardTitle }}
-      </v-toolbar-title>
-    </v-toolbar>
-    <v-card-text v-show="page == 'select'">
-      <v-row>
-        <v-col cols="4">
-          <v-list
-            density="compact"
-            class="wizard-list"
-          >
-            <v-list-subheader class="wizard-subheader">
-              OPERATION
-            </v-list-subheader>
-            <v-list-item
-              v-for="(name, i) in Object.keys(availableOperations)"
-              :key="i"
-              :value="name"
-              :title="name"
-              :active="name == selectedOperation"
-              class="wizard-operations"
-              @click="selectOperation(name)"
-            />
-          </v-list>
-        </v-col>
-        <v-col cols="8">
-          <v-card
-            :title="selectedOperation"
-            class="selected-operation"
-          >
-            <v-card-text>
-              <span class="operation-description">
-                {{ selectedOperationDescription }}
-              </span>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-card-text>
-    <v-slide-y-reverse-transition hide-on-leave>
-      <v-card-text
-        v-show="page == 'configure'"
-        class="wizard-card"
-      >
-        <div
-          v-for="(inputDescription, inputKey) in selectedOperationInputs"
-          :key="inputKey"
-          class="operation-input-wrapper"
-        >
-          <v-text-field
-            v-if="inputDescription.type !== 'file'"
-            v-model="selectedOperationInput[inputKey]"
-            :label="inputDescription.name"
-            :type="inputDescription.type"
-            class="operation-input"
-          />
-          <div
-            v-else-if="inputDescription.type == 'file'"
-            class="images-container"
-          >
-            <image-grid 
-              :data="data"
-              :column-span="calculateColumnSpan(data.input_data.length, imagesPerRow)"
-              :selected-images="selectedDataSessionImages"
-              class="wizard-images"
-              @image-clicked="handleThumbnailClick"
-            />
-          </div>
-        </div>
-      </v-card-text>
-    </v-slide-y-reverse-transition>
+	<v-card class="wizard-background">
+		<v-toolbar class="wizard-toolbar">
+			<v-toolbar-title class="wizard-title">
+				{{ wizardTitle }}
+			</v-toolbar-title>
+		</v-toolbar>
+		<v-card-text v-show="page == 'select'">
+			<v-row>
+				<v-col cols="4">
+					<v-list density="compact" class="wizard-list">
+						<v-list-subheader class="wizard-subheader">
+							OPERATION
+						</v-list-subheader>
+						<v-list-item v-for="(name, i) in Object.keys(availableOperations)" :key="i" :value="name" :title="name"
+							:active="name == selectedOperation" class="wizard-operations" @click="selectOperation(name)" />
+					</v-list>
+				</v-col>
+				<v-col cols="8">
+					<v-card :title="selectedOperation" class="selected-operation">
+						<v-card-text>
+							<span class="operation-description">
+								{{ selectedOperationDescription }}
+							</span>
+						</v-card-text>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-card-text>
+		<v-slide-y-reverse-transition hide-on-leave>
+			<v-card-text v-show="page == 'configure'" class="wizard-card">
+				<div v-for="(inputDescription, inputKey) in selectedOperationInputs" :key="inputKey"
+					class="operation-input-wrapper">
+					<v-text-field v-if="inputDescription.type !== 'file'" v-model="selectedOperationInput[inputKey]"
+						:label="inputDescription.name" :type="inputDescription.type" class="operation-input" />
+					<div v-else-if="inputDescription.type == 'file'" class="images-container">
+						<image-grid :images="images" :column-span="calculateColumnSpan(images.length, imagesPerRow)"
+							class="wizard-images" :allow-selection="true" @select-image="selectImage(inputKey, $event)" />
+					</div>
+				</div>
+			</v-card-text>
+		</v-slide-y-reverse-transition>
 
-    <v-card-actions class="buttons-container">
-      <v-spacer />
-      <v-btn
-        variant="text"
-        class="goback-btn"
-        @click="goBack"
-      >
-        Go Back
-      </v-btn>
-      <v-btn
-        variant="text"
-        class="gofwd-btn"
-        @click="goForward"
-      >
-        {{ goForwardText }}
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+		<v-card-actions class="buttons-container">
+			<v-spacer />
+			<v-btn variant="text" class="goback-btn" @click="goBack">
+				Go Back
+			</v-btn>
+			<v-btn variant="text" class="gofwd-btn" @click="goForward">
+				{{ goForwardText }}
+			</v-btn>
+		</v-card-actions>
+	</v-card>
 </template>
 
 <style scoped>
-.wizard-background{
-  background-color: var(--dark-blue);
-  height: 100vh;
+.wizard-background {
+	background-color: var(--dark-blue);
+	height: 100vh;
 }
+
 .wizard-toolbar {
-  background-color: var(--metal);
+	background-color: var(--metal);
 }
+
 .wizard-title {
-  color: var(--tan);
-  font-family: 'Open Sans', sans-serif;
-  font-weight: 600;
-  font-size: 1.7rem;
-  letter-spacing: 0.05rem;
-  margin-left: 2%;
+	color: var(--tan);
+	font-family: 'Open Sans', sans-serif;
+	font-weight: 600;
+	font-size: 1.7rem;
+	letter-spacing: 0.05rem;
+	margin-left: 2%;
 }
-.wizard-list{
-  background-color: var(--metal);
+
+.wizard-list {
+	background-color: var(--metal);
 }
+
 .wizard-subheader {
-  color: var(--tan);
-  font-weight: 500;
-  letter-spacing: 0.05rem;
-  font-size: 1.4rem;
+	color: var(--tan);
+	font-weight: 500;
+	letter-spacing: 0.05rem;
+	font-size: 1.4rem;
 }
+
 .wizard-operations {
-  color: var(--tan);
+	color: var(--tan);
 }
+
 .selected-operation {
-  color: var(--tan);
-  background-color: var(--metal);
-  text-transform: uppercase;
-  font-family: 'Open Sans', sans-serif;
-  font-size: 3rem;
+	color: var(--tan);
+	background-color: var(--metal);
+	text-transform: uppercase;
+	font-family: 'Open Sans', sans-serif;
+	font-size: 3rem;
 }
-.operation-description{
-  font-size: 1rem;
+
+.operation-description {
+	font-size: 1rem;
 }
+
 .wizard-card {
-  width: 100%; 
-  display: flex;
-  flex-direction: column-reverse;
+	width: 100%;
+	display: flex;
+	flex-direction: column-reverse;
 }
+
 .images-container {
-  display: flex;
-  flex-wrap: wrap; 
-  justify-content: flex-start; 
-  width: 100%; 
-  padding-left: 2rem; 
-  padding-right: 2rem; 
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	width: 100%;
+	padding-left: 2rem;
+	padding-right: 2rem;
 }
+
 .wizard-images {
-  max-width: 100%; 
-  height: auto; 
-  box-sizing: border-box; 
-  cursor: pointer;
+	max-width: 100%;
+	height: auto;
+	box-sizing: border-box;
+	cursor: pointer;
 }
+
 .selected-image {
-  border: 0.3rem solid var(--dark-green);
+	border: 0.3rem solid var(--dark-green);
 }
+
 .operation-input {
-  width: 10vw;
-  margin-left: 2%;
-  margin-bottom: 2%;
-  background-color: var(--metal);
+	width: 10vw;
+	margin-left: 2%;
+	margin-bottom: 2%;
+	background-color: var(--metal);
 }
+
 .buttons-container {
-  position: fixed;
-  right: 2rem;
-  bottom: 2rem;
+	position: fixed;
+	right: 2rem;
+	bottom: 2rem;
 }
+
 .goback-btn {
-  color: var(--cancel);
-  font-size: 1.2rem;
+	color: var(--cancel);
+	font-size: 1.2rem;
 }
-.gofwd-btn{
-  color:var(--light-blue);
-  font-size: 1.2rem;
+
+.gofwd-btn {
+	color: var(--light-blue);
+	font-size: 1.2rem;
 }
+
 @media (max-width: 1200px) {
-.operation-input {
-  margin-left: 3%;
+	.operation-input {
+		margin-left: 3%;
+	}
+
+	.images-container {
+		margin-top: 2%;
+	}
 }
-.images-container {
-  margin-top: 2%;
-}
-}
+
 @media (max-width: 900px) {
-.selected-operation {
-  height: 120%;
-}
-.operation-description{
-  font-size: 1rem;
-}
-.operation-input {
-  margin-left: 4%;
-  width: 15vw;
-}
-.images-container {
-  margin-top: 3%;
-}
+	.selected-operation {
+		height: 120%;
+	}
+
+	.operation-description {
+		font-size: 1rem;
+	}
+
+	.operation-input {
+		margin-left: 4%;
+		width: 15vw;
+	}
+
+	.images-container {
+		margin-top: 3%;
+	}
 }
 </style>
