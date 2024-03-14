708afb6553a66bdae4453bd2d9483ef030c61f82
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-03-14 15:01:25 -0700

    styled navbar

--- src/components/Global/NavBar.vue
+++ src/components/Global/NavBar.vue
@@ -6,24 +6,46 @@ import ColorblindToggle from './ColorblindToggle.vue'
 <template>
   <div class="navbar-wrapper">
     <nav class="navbar">
-      <ul>
+      <ol>
         <li class="nav-item">
-          home
+          PROJECTS
         </li>
         <li class="nav-item">
-          not home
+          DATA SESSION
         </li>
-      </ul>
-      <colorblind-toggle />
+        <li class="nav-item">
+          <colorblind-toggle />
+        </li>
+      </ol>
     </nav>
   </div>
 </template>
 
 <style scoped>
 .navbar-wrapper {
-  display: inline
-}
-.nav-item {
   display: inline;
 }
+.navbar {
+  background-color: var(--metal);
+  display: flex;
+  align-items: center;
+  height: 65px;
+}
+ol {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  list-style: none;
+}
+.nav-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: var(--tan);
+  font-weight: 600;
+  font-size: 1.2rem;
+  padding: 0 1rem;
+}
+
 </style>
