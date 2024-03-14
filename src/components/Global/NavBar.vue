712ba37e632eba0ee5e385767d263adcfc3b2bae
Carolina Capetillo <carolinacapetilloc@gmail.com>
2024-03-14 14:41:46 -0700

    basic nav bar

---
+++ src/components/Global/NavBar.vue
@@ -0,0 +1,29 @@
+<script setup>
+import ColorblindToggle from './ColorblindToggle.vue'
+
+</script>
+
+<template>
+  <div class="navbar-wrapper">
+    <nav class="navbar">
+      <ul>
+        <li class="nav-item">
+          home
+        </li>
+        <li class="nav-item">
+          not home
+        </li>
+      </ul>
+      <colorblind-toggle />
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.navbar-wrapper {
+  display: inline
+}
+.nav-item {
+  display: inline;
+}
+</style>
