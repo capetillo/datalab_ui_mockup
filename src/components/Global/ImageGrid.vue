0cd4e8c9580f87247f85fbe63c4d71e809e6403f
Lloyd Dakin <taidakin99lol@gmail.com>
2024-07-22 11:54:13 -0700

    css changes

--- src/components/Global/ImageGrid.vue
+++ src/components/Global/ImageGrid.vue
@@ -83,29 +83,22 @@ watch(() => props.images, () => {
 </template>
 
 <style scoped>
-.image-grid-container {
-  display: flex;
-  max-width: 200px;
-  max-height: 200px;
-}
 .selected-image {
   border: 0.3rem solid var(--dark-green);
 }
 
 .image-text-overlay {
-  color: white;
+  color: var(--tan);
   font-weight: bold;
   margin-right: 5px;
   float: right;
 }
 .image-grid {
   max-width: 200px;
-  height: auto;
 }
 @media (max-width: 900px) {
 .image-grid {
   width: 20vw;
-  height: auto;
 }
 }
 </style>
