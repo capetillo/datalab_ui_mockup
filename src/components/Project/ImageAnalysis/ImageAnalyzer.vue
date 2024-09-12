2b39415743fe5cfa2ac845caacd80400b4b93f68
Jon <jnation@lco.global>
2024-09-12 00:24:01 +0000

    Add analysis tab opening on datasession images when double clicked

--- src/components/Project/ImageAnalysis/ImageAnalyzer.vue
+++ src/components/Project/ImageAnalysis/ImageAnalyzer.vue
@@ -103,8 +103,8 @@ function handleAnalysisOutput(response, action, action_callback){
             <p>Instrument: {{ image.instrument_id }}</p>
             <span>Filter: 
               <filter-badge
-                v-if="image.FILTER"
-                :filter="image.FILTER"
+                v-if="image.FILTER || image.filter"
+                :filter="image.FILTER || image.filter"
               />
             </span>
           </v-sheet>
