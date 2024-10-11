e050689205cd4f3fbb801e58a9f92178dd93529a
Lloyd Dakin <taidakin99lol@gmail.com>
2024-10-11 13:22:10 -0700

    replaced cite tags with full name, info button for fits header

--- src/utils/common.js
+++ src/utils/common.js
@@ -3,4 +3,19 @@ const calculateColumnSpan = (imageCount, imagesPerRow) => {
   return totalColumns
 }
 
-export { calculateColumnSpan }
+function siteIDToName(siteID) {
+  const siteIDMap = {
+    'COJ': 'Siding Spring Observatory',
+    'CPT': 'South African Astronomical Observatory',
+    'TFN': 'Teide Observatory',
+    'LSC': 'Cerro Tololo Inter-American Observatory',
+    'ELP': 'McDonald Observatory',
+    'OGG': 'Haleakala Observatory',
+    'TLV': 'Wise Observatory',
+    'NGQ': 'Ali Observatory'
+  }
+
+  return siteIDMap[siteID?.toUpperCase()] || siteID
+}
+
+export { calculateColumnSpan, siteIDToName }
