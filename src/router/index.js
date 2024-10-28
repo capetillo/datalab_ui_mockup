e68c2314fa392135a6d01c0ab47f1873380c0f7a
Lloyd Dakin <taidakin99lol@gmail.com>
2024-10-28 15:36:29 -0700

    add query params for start and end date

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: Registration
   },
   {
-    path: '/projects:observationId?:ra?:dec?',
+    path: '/projects:startDate?:endDate?:observationId?:ra?:dec?',
     name: 'ProjectView',
     component: ProjectView,
     beforeEnter: requireAuth,
